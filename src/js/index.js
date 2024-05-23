ad116e3b59167f9a2f47c1da975b086b83f24a02
TulioPortela <156851372+Tulio-Portela@users.noreply.github.com>
2024-05-23 20:43:00 +0000

    conclusion

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,31 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="secondsFive"><i className="far fa-clock"></i></div>
+            <div className="secondsFour">{props.Four}</div>
+            <div className="secondsThree">{props.Three}</div>
+            <div className="secondsTwo">{props.Two}</div>
+            <div className="secondsOne">{props.One}</div>
+        </div>
+    );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function(){
+    const four = Math.floor(counter / 1000) % 10;
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = counter % 10;
+    console.log(four, three, two, one);
+
+    //render your react application
+    ReactDOM.render(
+        <SimpleCounter Four={four} Three={three} Two={two} One={one} />,
+        document.querySelector("#app")
+    );
+
+    counter++;
+}, 1000);
