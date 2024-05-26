0b718ea2435aa8ce844fe477f77a445dd0178b6c
TulioPortela <156851372+Tulio-Portela@users.noreply.github.com>
2024-05-26 17:27:48 +0000

    boas práticas

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,35 @@
-// Importar React en el paquete
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Incluir tus estilos en el paquete de webpack
+
 import "../styles/index.css";
 
-function ContadorSimple(props) {
+function ContadorSimple({ uno, dos, tres, cuatro }) {
     return (
         <div className="contadorGrande">
             <div className="segundosCinco"><i className="far fa-clock"></i></div>
-            <div className="segundosCuatro">{props.cuatro}</div>
-            <div className="segundosTres">{props.tres}</div>
-            <div className="segundosDos">{props.dos}</div>
-            <div className="segundosUno">{props.uno}</div>
+            <div className="segundosCuatro">{cuatro}</div>
+            <div className="segundosTres">{tres}</div>
+            <div className="segundosDos">{dos}</div>
+            <div className="segundosUno">{uno}</div>
         </div>
     );
 }
 
 let contador = 0;
-setInterval(function(){
-    const cuatro = Math.floor(contador / 1000) % 10;
-    const tres = Math.floor(contador / 100) % 10;
-    const dos = Math.floor(contador / 10) % 10;
-    const uno = contador % 10;
-    console.log(cuatro, tres, dos, uno);
+setInterval(() => {
+    const tiempo = {
+        cuatro: Math.floor(contador / 1000) % 10,
+        tres: Math.floor(contador / 100) % 10,
+        dos: Math.floor(contador / 10) % 10,
+        uno: contador % 10,
+    };
+    console.log(tiempo.cuatro, tiempo.tres, tiempo.dos, tiempo.uno);
 
-    // Renderizar tu aplicación de React
+    
     ReactDOM.render(
-        <ContadorSimple cuatro={cuatro} tres={tres} dos={dos} uno={uno} />,
+        <ContadorSimple {...tiempo} />,
         document.querySelector("#app")
     );
 
